551352f57e5abe487476c8d393066277457ef578
Andrew De Ponte <cyphactor@gmail.com>
2010-12-08 21:29:36 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IrcArch::Application.config.secret_token = 'd49ddefa8498bd7d82bae6c8eb937b384e4dee3983dc4db2a4abb11ebfd267ae845b298085bdd236c2ce8e1ff44cc21315984ee771da2fea671d37ad5a3ce58d'
