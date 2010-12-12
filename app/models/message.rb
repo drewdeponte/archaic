e09502d770f0b2316c2bc3ee02b55162aa044a3f
Andrew De Ponte <cyphactor@gmail.com>
2010-12-11 22:15:53 -0800

    Added basic text searching functionality to the Message model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+  def self.search(terms)
+    find_by_sql(["select t.* from messages t where #{ (["(lower(t.body) like ? or lower(t.author) like ?)"] * terms.size).join(" or ") } order by t.created_at desc", *(terms * 2).sort])
+  end
 end
