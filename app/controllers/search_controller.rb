15a3dc03f9d09db5976fd786f3ec1ea7d6a529cb
Andrew De Ponte <cyphactor@gmail.com>
2010-12-11 23:41:29 -0800

    Did some cleanup on searh interface and results.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def search
     @search_res = []
-    @search_terms = nil
-    if params.has_key?(:search_terms)
+    @search_terms = []
+    @performed_search = false
+    if params.has_key?(:search_terms) && !params[:search_terms].empty?
+      @performed_search = true
       @search_terms = params[:search_terms].split
       @search_res = Message.search(@search_terms.collect{ |c| "%#{c.downcase}%" })
     end
