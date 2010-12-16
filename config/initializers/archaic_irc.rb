0f421e0d9336e7e71ce6f7789cd87324c03b1a11
Chad Youngberg <cyoungberg@realpractice.com>
2010-12-16 09:53:14 +0800

    added tbc command to list current tustin brewing co. beers

--- config/initializers/archaic_irc.rb
+++ config/initializers/archaic_irc.rb
@@ -5,4 +5,9 @@ Archaic.config do |c|
   c.irc_server = "irc.freenode.net"
   c.irc_channels = ["#archaic_dev"]
   c.ticket_url = "http://github.com/cyphactor/archaic/issues#issue/:ticket_id"
+  
+  c.brew_url = 'http://www.tustinbrewery.com/'
+  c.brew_regex = /<div class="brew-name">(.*?)<\/div>/
+  c.brews_to_hide = [/guest beers:/, /\*Check out our bottle list/]
+  
 end
