afd9f1a2f9ea73597fefc00104f7605b0d3bc01e
Andrew De Ponte <cyphactor@gmail.com>
2011-01-04 23:26:29 -0800

    Added support for custom untracked config/ArchaicConfig.rb file to allow for overriding default config options.

--- config/initializers/archaic_irc.rb
+++ config/initializers/archaic_irc.rb
@@ -1,5 +1,6 @@
 require 'archaic_config'
 
+# Set all the hard coded defaults
 Archaic.config do |c|
   c.irc_nick = "dev_arch_bot"
   c.irc_server = "irc.freenode.net"
@@ -9,5 +10,10 @@ Archaic.config do |c|
   c.brew_url = 'http://www.tustinbrewery.com/'
   c.brew_regex = /<div class="brew-name">(.*?)<\/div>/
   c.brews_to_hide = [/guest beers:/, /\*Check out our bottle list/]
-  
+end
+
+# Provide an opportunity for the defaults and additional configs to be set
+custom_conf = File.expand_path('../../ArchaicConfig.rb', __FILE__)
+if File.exists?(custom_conf)
+  require custom_conf
 end
